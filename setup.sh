565be53c1d4c4ff930fb08a768326b6b32a1e30d
David Bootle <d.bootle18@psaschool.org>
2019-11-13 08:34:54 -0800

    Updated launcher

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ chmod a+x ~/Desktop/SlotMachine.desktop
 chmod 700 ~/Desktop/SlotMachine.desktop
 chmod a+x ~/Python-Slots/slotmachine/launcher.sh
 chmod 700 ~/Python-Slots/slotmachine/launcher.sh
+gsettings set org.gnome.nautilus.preferences executable-text-activation launch
 sudo apt-get install python3-tk
 cd ~/Python-Slots/slotmachine/Python-Slots
