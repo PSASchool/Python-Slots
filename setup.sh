b653428e7ca725cf836c9363bf6e03b991d96cac
David Bootle <d.bootle18@psaschool.org>
2019-11-14 08:37:41 -0800

    Added comments

--- setup.sh
+++ setup.sh
@@ -1,13 +1,22 @@
+# activate venv
 cd ~/Python-Slots
 . slotmachine/bin/activate
-mv SlotMachine.desktop ~/Desktop/SlotMachine.desktop
+
+# install wiki
 sudo apt-get install pandoc
 pandoc -s -o ~/Desktop/README.html wiki.md --css https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css
+
+# install launcher
+mv SlotMachine.desktop ~/Desktop/SlotMachine.desktop
 chmod a+x ~/Desktop/SlotMachine.desktop
 chmod 700 ~/Desktop/SlotMachine.desktop
 chmod a+x ~/Python-Slots/slotmachine/launcher.sh
 chmod 700 ~/Python-Slots/slotmachine/launcher.sh
 gsettings set org.gnome.nautilus.preferences executable-text-activation launch
+
+# install tkinter
 sudo apt-get install python3-tk
+
+# install admin-tools
 mv ~/Python-Slots/admin-tools ~/admin-tools
 cd ~/Python-Slots/slotmachine/Python-Slots
