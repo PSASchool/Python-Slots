7d4794ff11e123806a556d2dd9d82e3dd2674dcc
David Bootle <d.bootle18@psaschool.org>
2019-11-14 08:26:59 -0800

    Added automatic wiki install to setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 cd ~/Python-Slots
 . slotmachine/bin/activate
 mv SlotMachine.desktop ~/Desktop/SlotMachine.desktop
-mv wiki.html ~/Desktop/README.html
+sudo apt-get install pandoc
+pandoc -s -o ~/Desktop/README.html wiki.md --css https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css
 chmod a+x ~/Desktop/SlotMachine.desktop
 chmod 700 ~/Desktop/SlotMachine.desktop
 chmod a+x ~/Python-Slots/slotmachine/launcher.sh
