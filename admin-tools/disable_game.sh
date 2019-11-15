657af35458d4f7527b9418208208a0deb52f4b03
David Bootle <d.bootle18@psaschool.org>
2019-11-15 05:51:46 -0800

    Updated disable script to use json

--- admin-tools/disable_game.sh
+++ admin-tools/disable_game.sh
@@ -2,10 +2,10 @@ echo "Enter the time followed by m for minutes and s for seconds. ex) Enter 10m 
 read -p "Time to disable (0 is indefinite): " t
 read -p "Reason for disable: " reason
 
-printf "true\n$reason\n$t" > ~/admin-tools/disabled
+printf "{'disabled': 'true', 'reason': '$reason', 'time': '$time'}" > ~/admin-tools/disabled
 if [ $t != 0 ]
 then
-    sh -c "sleep $t; echo 'false' > ~/admin-tools/disabled" &
+    sh -c "sleep $t; echo '{\"disabled\": \"false\"}' > ~/admin-tools/disabled" &
     echo "Disabled for $t minutes."
 else
     echo "Disabled. To reenable, run enable_game.sh."
