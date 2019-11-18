7b5ea9e3a156b3e2f36fe0abf2f0953cc25de4b1
David Bootle <d.bootle18@psaschool.org>
2019-11-18 06:48:09 -0800

    Fixed admin-tools

--- admin-tools/disable_game.sh
+++ admin-tools/disable_game.sh
@@ -2,7 +2,7 @@ echo "Enter the time followed by m for minutes and s for seconds. ex) Enter 10m 
 read -p "Time to disable (0 is indefinite): " t
 read -p "Reason for disable: " reason
 
-printf "{'disabled': 'true', 'reason': '$reason', 'time': '$time'}"" > ~/admin-tools/disabled
+printf "{'disabled': 'true', 'reason': '$reason', 'time': '$t'}" > ~/admin-tools/disabled
 if [ $t != 0 ]
 then
     sh -c "sleep $t; echo '{\"disabled\": \"false\"}' > ~/admin-tools/disabled" &
