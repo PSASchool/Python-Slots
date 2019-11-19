3e358a9f4df59e60f04df28b993739a526b213b0
David Bootle <d.bootle18@psaschool.org>
2019-11-19 08:12:10 -0800

    Updated disable script

--- admin-tools/disable_game.sh
+++ admin-tools/disable_game.sh
@@ -3,7 +3,7 @@ read -p "Time to disable (0 is indefinite): " t
 read -p "Reason for disable: " reason
 
 printf "{'disabled': 'true', 'reason': '$reason', 'time': '$t'}" > ~/admin-tools/disabled
-notify-send "Slot Machine" "Your game has been disabled by the administrators for $reason for $t."
+notify-send "Slot Machine" "Your game has been disabled by the administrators for $reason."
 
 if [ $t != 0 ]
 then
