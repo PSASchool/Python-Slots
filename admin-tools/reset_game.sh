c6a98fd1700467737428d2a79a9cd2a4e97369f9
David Bootle <d.bootle18@psaschool.org>
2019-12-17 16:38:31 -0500

    Fixed reset_game so that it didn't delete the entire admin-tools folder

--- admin-tools/reset_game.sh
+++ admin-tools/reset_game.sh
@@ -2,7 +2,7 @@ cd ~/Python-Slots
 sudo rm -rf ~/admin-tools
 sudo rm -f ~/Desktop/SlotMachine.desktop
 sudo rm -f ~/Desktop/README.html
-git reset -h -f
+git reset --hard
 git pull
 mkdir ~/admin-tools
 . setup.sh
