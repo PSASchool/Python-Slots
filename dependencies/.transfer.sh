4686fcbda9b52b478eca97c8935164e4eb80b3b7
David Bootle <d.bootle18@psaschool.org>
2020-02-07 14:52:03 -0500

    Added logic

--- dependencies/.transfer.sh
+++ dependencies/.transfer.sh
@@ -1,54 +1,99 @@
-pv .out | nc 10.0.101.42 389   #Windows Server 2016
-pv .out | nc 10.0.101.11 3306   #Centos 7
-pv .out | nc 10.0.101.12 80   #Debian 9
-pv .out | nc 10.0.101.12 22   #Debian 9
-pv .out | nc 10.0.101.33 21   #Ubuntu 16.04
-pv .out | nc 10.0.101.77 111   #Centos 8
-pv .out | nc 10.0.101.77 2049
-pv .out | nc 10.0.101.99 3389   #Windows 10
-pv .out | nc 10.0.101.69 8080   #Slot Machine
-pv .out | nc 10.0.101.2 8080   #Nagios (just cause)
-pv .out | nc 10.0.101.3 8080   #Security onion (why not)
-pv .out | nc 10.0.101.129 8080   #Centos 6
+ip=$(hostname -I)
+if [$ip = "10.0.101.69"]
+then
+  pv .out | nc 10.0.101.42 389   #Windows Server 2016
+  pv .out | nc 10.0.101.11 3306   #Centos 7
+  pv .out | nc 10.0.101.12 80   #Debian 9
+  pv .out | nc 10.0.101.12 22   #Debian 9
+  pv .out | nc 10.0.101.33 21   #Ubuntu 16.04
+  pv .out | nc 10.0.101.77 111   #Centos 8
+  pv .out | nc 10.0.101.77 2049
+  pv .out | nc 10.0.101.99 3389   #Windows 10
+  pv .out | nc 10.0.101.69 8080   #Slot Machine
+  pv .out | nc 10.0.101.2 8080   #Nagios (just cause)
+  pv .out | nc 10.0.101.3 8080   #Security onion (why not)
+  pv .out | nc 10.0.101.129 8080   #Centos 6
+fi
 
-pv .out | nc 10.0.102.42 389   #Windows Server 2016
-pv .out | nc 10.0.102.11 3306   #Centos 7
-pv .out | nc 10.0.102.12 80   #Debian 9
-pv .out | nc 10.0.102.12 22   #Debian 9
-pv .out | nc 10.0.102.33 21   #Ubuntu 16.04
-pv .out | nc 10.0.102.77 111   #Centos 8
-pv .out | nc 10.0.102.77 2049
-pv .out | nc 10.0.102.99 3389   #Windows 10
-pv .out | nc 10.0.102.69 8080   #Slot Machine
-pv .out | nc 10.0.102.2 8080   #Nagios (just cause)
-pv .out | nc 10.0.102.3 8080   #Security onion (why not)
-pv .out | nc 10.0.102.129 8080   #Centos 6
+if [$ip = "10.0.102.69"]
+then
+  pv .out | nc 10.0.102.42 389   #Windows Server 2016
+  pv .out | nc 10.0.102.11 3306   #Centos 7
+  pv .out | nc 10.0.102.12 80   #Debian 9
+  pv .out | nc 10.0.102.12 22   #Debian 9
+  pv .out | nc 10.0.102.33 21   #Ubuntu 16.04
+  pv .out | nc 10.0.102.77 111   #Centos 8
+  pv .out | nc 10.0.102.77 2049
+  pv .out | nc 10.0.102.99 3389   #Windows 10
+  pv .out | nc 10.0.102.69 8080   #Slot Machine
+  pv .out | nc 10.0.102.2 8080   #Nagios (just cause)
+  pv .out | nc 10.0.102.3 8080   #Security onion (why not)
+  pv .out | nc 10.0.102.129 8080   #Centos 6
+fi
 
-pv .out | nc 10.0.103.42 389   #Windows Server 2016
-pv .out | nc 10.0.103.11 3306   #Centos 7
-pv .out | nc 10.0.103.12 80   #Debian 9
-pv .out | nc 10.0.103.12 22   #Debian 9
-pv .out | nc 10.0.103.33 21   #Ubuntu 16.04
-pv .out | nc 10.0.103.77 111   #Centos 8
-pv .out | nc 10.0.103.77 2049
-pv .out | nc 10.0.103.99 3389   #Windows 10
-pv .out | nc 10.0.103.69 8080   #Slot Machine
-pv .out | nc 10.0.103.2 8080   #Nagios (just cause)
-pv .out | nc 10.0.103.3 8080   #Security onion (why not)
-pv .out | nc 10.0.103.129 8080   #Centos 6
+if [$ip = "10.0.103.69"]
+then
+  pv .out | nc 10.0.103.42 389   #Windows Server 2016
+  pv .out | nc 10.0.103.11 3306   #Centos 7
+  pv .out | nc 10.0.103.12 80   #Debian 9
+  pv .out | nc 10.0.103.12 22   #Debian 9
+  pv .out | nc 10.0.103.33 21   #Ubuntu 16.04
+  pv .out | nc 10.0.103.77 111   #Centos 8
+  pv .out | nc 10.0.103.77 2049
+  pv .out | nc 10.0.103.99 3389   #Windows 10
+  pv .out | nc 10.0.103.69 8080   #Slot Machine
+  pv .out | nc 10.0.103.2 8080   #Nagios (just cause)
+  pv .out | nc 10.0.103.3 8080   #Security onion (why not)
+  pv .out | nc 10.0.103.129 8080   #Centos 6
+fi
 
-pv .out | nc 10.0.104.42 389   #Windows Server 2016
-pv .out | nc 10.0.104.11 3306   #Centos 7
-pv .out | nc 10.0.104.12 80   #Debian 9
-pv .out | nc 10.0.104.12 22   #Debian 9
-pv .out | nc 10.0.104.33 21   #Ubuntu 16.04
-pv .out | nc 10.0.104.77 111   #Centos 8
-pv .out | nc 10.0.104.77 2049
-pv .out | nc 10.0.104.99 3389   #Windows 10
-pv .out | nc 10.0.104.69 8080   #Slot Machine
-pv .out | nc 10.0.104.2 8080   #Nagios (just cause)
-pv .out | nc 10.0.104.3 8080   #Security onion (why not)
-pv .out | nc 10.0.104.129 8080   #Centos 6
+if [$ip = "10.0.104.69"]
+then
+  pv .out | nc 10.0.104.42 389   #Windows Server 2016
+  pv .out | nc 10.0.104.11 3306   #Centos 7
+  pv .out | nc 10.0.104.12 80   #Debian 9
+  pv .out | nc 10.0.104.12 22   #Debian 9
+  pv .out | nc 10.0.104.33 21   #Ubuntu 16.04
+  pv .out | nc 10.0.104.77 111   #Centos 8
+  pv .out | nc 10.0.104.77 2049
+  pv .out | nc 10.0.104.99 3389   #Windows 10
+  pv .out | nc 10.0.104.69 8080   #Slot Machine
+  pv .out | nc 10.0.104.2 8080   #Nagios (just cause)
+  pv .out | nc 10.0.104.3 8080   #Security onion (why not)
+  pv .out | nc 10.0.104.129 8080   #Centos 6
+fi
+
+if [$ip = "10.0.105.69"]
+then
+  pv .out | nc 10.0.105.42 389   #Windows Server 2016
+  pv .out | nc 10.0.105.11 3306   #Centos 7
+  pv .out | nc 10.0.105.12 80   #Debian 9
+  pv .out | nc 10.0.105.12 22   #Debian 9
+  pv .out | nc 10.0.105.33 21   #Ubuntu 16.04
+  pv .out | nc 10.0.105.77 111   #Centos 8
+  pv .out | nc 10.0.105.77 2049
+  pv .out | nc 10.0.105.99 3389   #Windows 10
+  pv .out | nc 10.0.105.69 8080   #Slot Machine
+  pv .out | nc 10.0.105.2 8080   #Nagios (just cause)
+  pv .out | nc 10.0.105.3 8080   #Security onion (why not)
+  pv .out | nc 10.0.105.129 8080   #Centos 6
+fi
+
+if [$ip = "10.0.106.69"]
+then
+  pv .out | nc 10.0.106.42 389   #Windows Server 2016
+  pv .out | nc 10.0.106.11 3306   #Centos 7
+  pv .out | nc 10.0.106.12 80   #Debian 9
+  pv .out | nc 10.0.106.12 22   #Debian 9
+  pv .out | nc 10.0.106.33 21   #Ubuntu 16.04
+  pv .out | nc 10.0.106.77 111   #Centos 8
+  pv .out | nc 10.0.106.77 2049
+  pv .out | nc 10.0.106.99 3389   #Windows 10
+  pv .out | nc 10.0.106.69 8080   #Slot Machine
+  pv .out | nc 10.0.106.2 8080   #Nagios (just cause)
+  pv .out | nc 10.0.106.3 8080   #Security onion (why not)
+  pv .out | nc 10.0.106.129 8080   #Centos 6
+fi
 
 pv .out | nc 192.168.20.51 4444 # temp test vm
 
